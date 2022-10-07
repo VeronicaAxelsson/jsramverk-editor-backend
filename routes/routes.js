af9d595c7438dc86079b0e30835fe3b9bf173352
Your Name <veronica-a@live.se>
2022-10-07 13:22:51 +0100

    auth middleware

--- routes/routes.js
+++ routes/routes.js
@@ -6,8 +6,8 @@ const authController = require('../controllers/authController');
 
 const routes = express.Router();
 
-routes.use('/docs', docsRouter);
-routes.use('/user', userRouter);
+routes.use('/docs', authController.checkToken, docsRouter);
+routes.use('/user', authController.checkToken, userRouter);
 routes.use('/auth', authRouter);
 
 module.exports = routes;
